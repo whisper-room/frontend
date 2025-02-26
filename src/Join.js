b4e92ccb483d386b75d264761885226084faa28a
hhyukk <hhyukk1273@gmail.com>
2025-02-27 03:20:35 +0900

    feat: 프로필 업로드시 프로필 미리보기 기능 추가 - useState를 사용하여 이미지를 imgFile 상태에 추가 - imgFile이 존재하면 .join_img_div에 이미지 표시

--- src/Join.js
+++ src/Join.js
@@ -1,34 +1,48 @@
 import styles from './css/Join.module.css';
 import { IoCameraOutline } from 'react-icons/io5';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 function Join() {
   const fileInputRef = useRef(null);
+  const [imgFile, setImgFile] = useState('');
+
+  const saveImgFile = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setImgFile(reader.result);
+      };
+    }
+  };
 
   const handleIconClick = () => {
     fileInputRef.current.click();
   };
+
   return (
     <div>
       <h1 className={styles.join_h1}>회원가입</h1>
       <div className={styles.join_img_div}>
+        {imgFile ? <img src={imgFile} alt="프로필 이미지" className={styles.join_image} /> : null}
         <div className={styles.join_icon_div} onClick={handleIconClick}>
           <IoCameraOutline className={styles.join_camera} />
         </div>
-        <input type="file" ref={fileInputRef} className={styles.hidden_input} accept="image/*" />
+        <input type="file" ref={fileInputRef} className={styles.hidden_input} accept="image/*" onChange={saveImgFile} />
       </div>
       <form className={styles.join_form}>
         <div className={styles.join_input_div}>
           <label className={styles.join_label}>닉네임</label>
-          <input type="text" className={styles.join_input}></input>
+          <input type="text" className={styles.join_input} />
         </div>
         <div className={styles.join_input_div}>
           <label className={styles.join_label}>비밀번호</label>
-          <input type="password" className={styles.join_input_password}></input>
+          <input type="password" className={styles.join_input_password} />
         </div>
         <div className={styles.join_input_div}>
           <label className={styles.join_label}>이메일</label>
-          <input type="email" className={styles.join_input}></input>
+          <input type="email" className={styles.join_input} />
         </div>
         <button className={styles.join_button} type="submit">
           가입하기
