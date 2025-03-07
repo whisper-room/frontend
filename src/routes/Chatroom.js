62f8771da12b234243d2306a1ad955e1ee17c320
hhyukk <hhyukk1273@gmail.com>
2025-03-07 23:18:41 +0900

    feat: chatroom header 추가

--- src/routes/Chatroom.js
+++ src/routes/Chatroom.js
@@ -1,4 +1,13 @@
+import styles from '../css/Chatroom.module.css';
+
 function Chatroom() {
-  return <h1>Chatroom</h1>;
+  return (
+    <div>
+      <div className={styles.header}>
+        <h1>whisper room</h1>
+        <button>로그아웃</button>
+      </div>
+    </div>
+  );
 }
 export default Chatroom;
