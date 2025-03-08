1a0354d81db1b0e31dade47df2bda57c1be5cee8
hhyukk <hhyukk1273@gmail.com>
2025-03-08 20:01:31 +0900

    feat: 로그아웃 API 추가

--- src/routes/Chatroom.js
+++ src/routes/Chatroom.js
@@ -1,14 +1,34 @@
 import styles from '../css/Chatroom.module.css';
+import { useNavigate } from 'react-router-dom';
 import { IoSearch } from 'react-icons/io5';
 import { FaPlus } from 'react-icons/fa6';
 import { FaPen } from 'react-icons/fa';
 
 function Chatroom() {
+  const navigate = useNavigate();
+  const handleClick = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/logout', {
+        method: 'POST',
+      });
+
+      const result = await response.json();
+
+      if (response.ok) {
+        alert(result.message);
+        navigate('/');
+      } else {
+        alert(result.message);
+      }
+    } catch (error) {
+      console.error('Login Error:', error);
+    }
+  };
   return (
     <div>
       <div className={styles.header}>
         <h1>whisper room</h1>
-        <button>로그아웃</button>
+        <button onClick={handleClick}>로그아웃</button>
       </div>
       <div className={styles.chatroom_container}>
         <div>
